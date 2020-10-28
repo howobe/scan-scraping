cc8d9b4d9cbadaa84a7f6d7bf21e8a8b83720289
howobe <howobe@howobe.howobe>
2020-10-28 16:21:04 +0000

    added info files

---
+++ README.md
@@ -0,0 +1,4 @@
+# scan-computing-scraper WIP
+The purpose of this repository is to provide the framework for gathering data
+ regarding [Scan Computing](https://scan.co.uk), and eventually providing services
+ for acting upon this information.
---
+++ dicttools.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# {cat: [{}, {}, {}, ...]}
+from typing import TypeVar
+
+T = TypeVar('T')
+
+
+class DictTools():
+
+    @staticmethod
+    def flatten(dictionary: dict, newKeyName: str) -> list:
+        newArr = []
+        for k, v in dictionary.items():
+            newArr.extend([{**vi, **{newKeyName: k}} for vi in v])
+        return newArr
+
+    @staticmethod
+    def printDict(dictionary: dict, keys=False, values=False,
+                  keyName: T = None):
+        ks, vs = dictionary.keys(), dictionary.values()
+        if keys == values:
+            for k, v in zip(ks, vs):
+                print(f"{k}:{v}")
+        elif keys:
+            for k in ks:
+                print(k)
+        elif values:
+            for v in vs:
+                print(v)
+        elif keyName is not None:
+            for v in dictionary[keyName]:
+                print(v)
