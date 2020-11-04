7e7d23e4ba0aaf762db98d0835ae4c8670aef438
howobe <howobe@howobe.howobe>
2020-11-04 16:22:56 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(install_requires=["selenium",
+                        "slackclient",
+                        "bs4",
+                        "pandas"])
